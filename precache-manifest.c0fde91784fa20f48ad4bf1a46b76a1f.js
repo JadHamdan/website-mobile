6c302e5a40d5835c738f7018ee4d6c9a9a2233d9
Jad Hamdan <jad.hamdan@mail.mcgill.ca>
2019-08-13 14:59:53 +0300

    first commit for mobile

---
+++ precache-manifest.c0fde91784fa20f48ad4bf1a46b76a1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fa8ba025f425ab61632077074a752c",
+    "url": "/website-mobile/index.html"
+  },
+  {
+    "revision": "4186dd451917cb548bd8",
+    "url": "/website-mobile/static/css/main.7ade8445.chunk.css"
+  },
+  {
+    "revision": "8833c0abcf683c4ece85",
+    "url": "/website-mobile/static/js/2.6ec87014.chunk.js"
+  },
+  {
+    "revision": "4186dd451917cb548bd8",
+    "url": "/website-mobile/static/js/main.8b179185.chunk.js"
+  },
+  {
+    "revision": "fc7d92fe0659aaa45ee3",
+    "url": "/website-mobile/static/js/runtime~main.f767a64f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website-mobile/static/media/logo.5d5d9eef.svg"
+  }
+]);
